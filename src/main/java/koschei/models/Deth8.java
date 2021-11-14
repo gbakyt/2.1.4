93743af02f6e257501cc93b5dca1423580b1d9e8
Gaziz <gazizbakhyt@gmail.com>
2021-11-14 10:31:20 +0600

    m

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,11 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+
+
+    final private Island2 island2;
+
+    @Autowired
+    public Ocean1(Island2 island2) {
+        this.island2 = island2;
+    }
+    @Override
+    public String toString() {
+        return "на океане остров " + island2.toString();
+    }
+}
